cd56b0a8134b5a0d4bd75f71bac8c90cb9c1eb5b
Chirkov Maxim <maxim.chirkov@skyeng.ru>
2022-06-08 16:33:20 +0700

    first-commit

---
+++ src/main/java/pro/sky/hw_2_3/Hw23Application.java
@@ -0,0 +1,14 @@
+package pro.sky.hw_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw23Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Hw23Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw_2_3/Hw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
